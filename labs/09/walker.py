bcbfcbb5f2f0d2ed61c1f327b2d0b9b3aed4a447
Viktor Cihal <viktor.cihal@gmail.com>
2024-04-27 21:56:54 +0200

    Add wandb flag

--- labs/09/walker.py
+++ labs/09/walker.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--env", default="BipedalWalker-v3", type=str, help="Environment.")
 parser.add_argument("--recodex", default=True, action="store_true", help="Running in ReCodEx")
-parser.add_argument("--render_each", default=1, type=int, help="Render some episodes.")
+parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
