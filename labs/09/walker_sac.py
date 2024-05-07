45ade7aed75d691544f96b04cc4fd19fa9ee82ed
Viktor Cihal <viktor.cihal@gmail.com>
2024-05-07 10:09:01 +0200

    Set hardcore

--- labs/09/walker_sac.py
+++ labs/09/walker_sac.py
@@ -12,9 +12,9 @@ import wrappers
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--env", default="BipedalWalker-v3", type=str, help="Environment.")
+parser.add_argument("--env", default="BipedalWalkerHardcore-v3", type=str, help="Environment.")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
-parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+parser.add_argument("--render_each", default=100, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
