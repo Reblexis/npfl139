f2ea6441be096bf3b45947627e56ea6334359be9
Viktor Cihal <viktor.cihal@gmail.com>
2024-05-12 22:09:47 +0200

    Change to better

--- labs/09/walker_sac.py
+++ labs/09/walker_sac.py
@@ -13,8 +13,8 @@ import wrappers
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--env", default="BipedalWalkerHardcore-v3", type=str, help="Environment.")
-parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
-parser.add_argument("--render_each", default=100, type=int, help="Render some episodes.")
+parser.add_argument("--recodex", default=True, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--render_each", default=1, type=int, help="Render some episodes.")
 parser.add_argument("--seed", default=None, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 # For these and any other arguments you add, ReCodEx will keep your default value.
@@ -289,7 +289,7 @@ def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
 
     # Evaluation in ReCodEx
     if args.recodex:
-        random_value = 447431
+        random_value = 103321
         network.load_actor(f"{args.env}_{random_value}")
         while True:
             evaluate_episode(True)
